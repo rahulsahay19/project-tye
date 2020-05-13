49b48d89ccc7ea3c9d6f714c7063f637c2d3b4c3
Rahul Sahay <rahulsahay19@hotmail.com>
2020-05-14 02:14:16 +0530

    allowed any origin

--- MoviesAPI/Startup.cs
+++ MoviesAPI/Startup.cs
@@ -32,7 +32,7 @@ namespace MoviesAPI
             {
                 options.AddPolicy("CorsPolicy", policy =>
                 {
-                   policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
+                   policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
                 });
             });
         }
